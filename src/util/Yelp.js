d0feade60663070860b43e94b84b0b6cfb6b3829
Sanford Huynh <huynhsanford@gmail.com>
2020-11-13 21:10:04 -0800

    Ravenous project

---
+++ src/util/Yelp.js
@@ -0,0 +1,36 @@
+const apiKey =
+  "cya8ViNQYjs4G7CDtky3X1tKF9qNqha0n06bUeKNF8qkp-ZTOtagOzVLJDlkMuUEFlnqAj8_42pqy2r1fdzkGX_Recaca6RFa4t7VPDdrq0DBWhYW3dEYPmOsvGAX3Yx";
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map((business) => ({
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count,
+          }));
+        }
+      });
+  },
+};
+
+export default Yelp;
